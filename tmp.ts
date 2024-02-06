0e68cd6842a5d6506eaf4f031446d2df31b53eec
heyjun3 <t3k2g5kako@yahoo.co.jp>
2024-02-06 22:52:06 +0900

    fix types

---
+++ tmp.ts
@@ -0,0 +1,23 @@
+class User {
+    private readonly name: string;
+    private readonly age:number; 
+
+    constructor(name: string, age: number) {
+        this.name = name
+        this.age = age
+    }
+
+    showAll() {
+        const a = [this.name, this.age].map((v) => {
+            console.warn(v.toUpperCase())
+        })
+    }
+}
+
+function getEntries<T extends object>(obj: T): [keyof T, T[keyof T]][] {
+  return Object.entries(obj) as [keyof T, T[keyof T]][];
+}
+
+const user = new User('tom', 5)
+user.showAll()
+
